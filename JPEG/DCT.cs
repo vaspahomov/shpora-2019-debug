af4d993d11c6c8da5113b0d14afb0c3fdc63d9cf
vaspahomov <vas2142553@gmail.com>
2019-03-11 19:05:53 +0500

    убрал MathEx

--- JPEG/DCT.cs
+++ JPEG/DCT.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using JPEG.Utilities;
 
 namespace JPEG
 {
@@ -10,20 +9,17 @@ namespace JPEG
             var height = input.GetLength(0);
             var width = input.GetLength(1);
             var coeffs = new double[width, height];
-
-            MathEx.LoopByTwoVariables(
-                0, width,
-                0, height,
-                (u, v) =>
-                {
-                    var sum = MathEx
-                        .SumByTwoVariables(
-                            0, width,
-                            0, height,
-                            (x, y) => input[x, y] * BasisFunction(u, v, x, y, height, width));
-
-                    coeffs[u, v] = sum * Beta(height, width) * Alpha(u) * Alpha(v);
-                });
+            
+            for (var x = 0; x < width; x++)
+            for (var y = 0; y < height; y++)
+            {
+                var sum = 0.0;
+                for (var u = 0; u < width; u++)
+                for (var v = 0; v < height; v++)
+                    sum += input[u, v] * BasisFunction(x, y, u, v, height, width);
+                
+                coeffs[x, y] = sum * Beta(height, width) * Alpha(x) * Alpha(y);
+            }
 
             return coeffs;
         }
@@ -33,12 +29,11 @@ namespace JPEG
             for (var x = 0; x < coeffs.GetLength(1); x++)
             for (var y = 0; y < coeffs.GetLength(0); y++)
             {
-                var sum = MathEx
-                    .SumByTwoVariables(
-                        0, coeffs.GetLength(1),
-                        0, coeffs.GetLength(0),
-                        (u, v) => coeffs[u, v] * BasisFunction(u, v, x, y, coeffs.GetLength(0), coeffs.GetLength(1)) *
-                                  Alpha(u) * Alpha(v));
+                var sum = 0.0;
+                for (var u = 0; u < coeffs.GetLength(1); u++)
+                for (var v = 0; v < coeffs.GetLength(0); v++)
+                    sum+=coeffs[u, v] * BasisFunction(u, v, x, y, coeffs.GetLength(0), coeffs.GetLength(1)) *
+                        Alpha(u) * Alpha(v);
 
                 output[x, y] = sum * Beta(coeffs.GetLength(0), coeffs.GetLength(1));
             }
@@ -52,12 +47,16 @@ namespace JPEG
             return b * c;
         }
 
+        
+        private static readonly double alphaCoef = 1 / Math.Sqrt(2);
+
         private static double Alpha(int u)
         {
             if (u == 0)
-                return 1 / Math.Sqrt(2);
+                return alphaCoef;
             return 1;
         }
+
 
         private static double Beta(int height, int width)
         {
